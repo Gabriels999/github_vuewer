f887c8b0b03975b32bcadc49d628f3c5d3610052
Gabriel Silva Santos <gabriel.santos@buser.com.br>
2023-01-25 09:55:50 -0300

    refactoring: breadcrumb matching treeview

---
+++ src/components/NavPath.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-breadcrumbs :items="items">
+    <template v-slot:divider>
+      <v-icon>mdi-chevron-right</v-icon>
+    </template>
+  </v-breadcrumbs>
+</template>
+<script>
+export default {
+  props: ["path"],
+  data: () => ({
+    items: [],
+  }),
+  watch: {
+    path() {
+      this.items = this.path;
+    },
+  },
+};
+</script>
--- src/components/RepoFiles.vue
+++ src/components/RepoFiles.vue
@@ -1,27 +1,35 @@
 <template>
-  <v-treeview v-model="tree" :items="repoFiles" activatable item-key="name" open-on-click>
-    <template v-slot:prepend="{ item, open }">
-      <v-icon v-if="!item.file">
-        {{ open ? "mdi-folder-open" : "mdi-folder" }}
-      </v-icon>
-      <v-icon v-else>
-        {{ files[item.file] }}
-      </v-icon>
-    </template>
-  </v-treeview>
+  <div>
+    <NavPath :path="path" />
+    <v-treeview v-model="tree" :items="repoFiles" activatable @update:active="setPath" item-key="name" open-on-click>
+      <template v-slot:prepend="{ item, open }">
+        <v-icon v-if="!item.file">
+          {{ open ? "mdi-folder-open" : "mdi-folder" }}
+        </v-icon>
+        <v-icon v-else>
+          {{ files[item.file] }}
+        </v-icon>
+      </template>
+    </v-treeview>
+  </div>
 </template>
 
 <script>
+import NavPath from "@/components/NavPath.vue";
 import { API } from "@/helpers/api";
 import { icons } from "@/assets/icons";
 
 export default {
   props: ["repoLink"],
+  components: {
+    NavPath,
+  },
   data: () => ({
     files: {},
     tree: [],
     repoFiles: [],
     selectedRepoLink: "",
+    path: [],
   }),
   methods: {
     async getFiles() {
@@ -29,6 +37,19 @@ export default {
       const data = await API.fetchRepoData(this.repoLink);
       this.searchingFiles = false;
       return data;
+    },
+    setPath(arr) {
+      debugger;
+      this.path = [
+        {
+          text: "/",
+          disabled: true,
+        },
+      ];
+      this.path.push({
+        text: arr[0],
+        disabled: true,
+      });
     },
   },
   watch: {
